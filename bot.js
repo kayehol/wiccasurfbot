3e445c456c6c5029c6221e632734b03601c35ffa
kayehol <kaye.holanda@gmail.com>
2020-05-09 11:27:37 -0300

    bot funcionando localmente

--- bot.js
+++ bot.js
@@ -1,38 +1,39 @@
-const twit = require('twit');
-var app = require("./server.js")
+const twit = require("twit");
+/*var app = require("./server.js");
 
 //Configura a porta disponível ou a porta 3000
 var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
 //Configura o host disponível ou "0.0.0.0"
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
+var server_host = process.env.YOUR_HOST || "0.0.0.0";
+*/
 
 const T = new twit({
-    consumer_key: process.env.consumer_key,
-    consumer_secret: process.env.consumer_secret,
-    access_token: process.env.access_token,
-    access_token_secret: process.env.access_token_secret
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret,
 });
 
-app.listen(server_port, server_host, function () {
-    console.log("Aplicação online.");
-});
+function retweet() {
+  let params = {
+    q: "wicca surf",
+    count: 10,
+  };
+  T.get("search/tweets", params, (err, data, response) => {
+    let tweets = data.statuses;
 
-// start stream and track tweets
-
-// use this to log errors from requests
-function responseCallback(err, data, response) {
-  console.log(err);
+    if (!err) {
+      for (let dat of tweets) {
+        let retweetId = dat.id_str;
+        T.post("statuses/retweet/:id", { id: retweetId }, (err, response) => {
+          if (response) console.log("Retuitado " + retweetId);
+          if (err)
+            console.log(
+              "Algo errado no RT"
+            );
+        });
+      }
+    }
+  });
 }
-
-
-// event handler
-
-
-app.get("/", function (req, res) {
-    const stream = T.stream('statuses/filter', { track: 'wicca surf' });
-
-    stream.on('tweet', tweet => {
-        // retweet
-        T.post('statuses/retweet/:id', { id: tweet.id_str }, responseCallback);
-      });
-});
+setInterval(retweet, 10000);
